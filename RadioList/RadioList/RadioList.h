6282ab1ca572398fd95ce1e60bdc79822b17e960
ronb <ron@hotelmize.com>
2016-04-30 09:52:47 +0300

    RadioList

---
+++ RadioList/RadioList/RadioList.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "CheckList.h"
+
+class RadioList : public CheckList
+{
+public:
+	RadioList(string* textList, int listSize, short width, short hieght, DWORD color);
+	~RadioList();
+	void Mark();
+	string GetInput();
+	int LongestOptios();
+};
+
